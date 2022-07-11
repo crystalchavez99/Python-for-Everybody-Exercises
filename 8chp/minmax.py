2a30bb8f5f9887bf567a8146d858074161369140
Crystal <crystal120599@gmail.com>
2022-07-11 10:58:30 -0700

    modd 8.6

--- 8chp/minmax.py
+++ 8chp/minmax.py
@@ -1,3 +1,6 @@
+# Write the program to store the
+# numbers the user enters in a list and use the max() and min() functions to
+# compute the maximum and minimum numbers after the loop completes. end when the user enters “done”
 number = input("Enter a number: ")
 numList = list()
 while (number != "done"):
